a2bf5b033caf5b0b8abe25c8f7c157e5b32feba9
NahomKibreab <nknahom@gmail.com>
2021-01-21 16:25:29 -0700

    Express routing basics

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-  console.log('New Request Recieved!');
-  res.send('<h1>Welcome to Express Node.JS</h1>');
+// app.use((req, res) => {
+//   console.log('New Request Recieved!');
+//   res.send('<h1>Welcome to Express Node.JS</h1>');
+// });
+
+app.get('/', (req, res) => {
+  res.send('<h2>Home Page</h2>');
+});
+
+app.get('/cats', (req, res) => {
+  res.send('<h2>Cats Page</h2>');
+});
+
+app.get('/dogs', (req, res) => {
+  res.send('<h2>Dogs Page</h2>');
+});
+
+app.get('*', (req, res) => {
+  res.send('<h2>This page is not found</h2>');
 });
 
 app.listen(8080, () => {
