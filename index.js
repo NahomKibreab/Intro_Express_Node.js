ce160c2e0721b646101ea97d1a04224d2dc1e479
NahomKibreab <nknahom@gmail.com>
2021-01-21 16:08:13 -0700

    Add express req and res objects params

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
   console.log('New Request Recieved!');
+  res.send('<h1>Welcome to Express Node.JS</h1>');
 });
 
 app.listen(8080, () => {
